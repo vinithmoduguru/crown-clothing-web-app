46f733a8dbe02e51b20367d8bcdd0e916d0605fb
vinithmoduguru <vinith1142@gmail.com>
2025-02-03 09:27:27 +0530

    Installation with vite, tailwind and shadcn

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div className="container bg-amber-50 mx-auto">
-      <h1>Hello Crown Clothing App</h1>
-    </div>
-  );
-};
-
-export default App;
--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,8 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
