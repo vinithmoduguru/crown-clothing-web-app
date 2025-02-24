b6f087149648630e64b4f6bfa0cfb75adc63a87a
vinithmoduguru <vinith1142@gmail.com>
2025-02-24 10:22:27 +0530

    Added Authentication

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -6,23 +6,41 @@ import { CartProvider } from "./context/cartContext";
 import Checkout from "./container/checkout/checkout";
 import PaymentCancel from "./container/payment/cancel";
 import PaymentSucess from "./container/payment/success";
+import {
+  SignedOut,
+  SignInButton,
+  SignedIn,
+  UserButton,
+  useAuth,
+} from "@clerk/clerk-react";
 
 export default function App() {
+  const { isSignedIn } = useAuth();
   return (
     <header>
-      <CartProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigation />}>
-              <Route index element={<Home />} />
-              <Route path="shop/*" element={<Shop />} />
-              <Route path="checkout" element={<Checkout />} />
-              <Route path="payment/cancel" element={<PaymentCancel />} />
-              <Route path="payment/success" element={<PaymentSucess />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </CartProvider>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      {isSignedIn ? (
+        <CartProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+                <Route path="shop/*" element={<Shop />} />
+                <Route path="checkout" element={<Checkout />} />
+                <Route path="payment/cancel" element={<PaymentCancel />} />
+                <Route path="payment/success" element={<PaymentSucess />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </CartProvider>
+      ) : (
+        <></>
+      )}
     </header>
   );
 }
--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -3,13 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env.local file");
+}
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={"/"}>
+        <App />
+      </ClerkProvider>
     </QueryClientProvider>
   </StrictMode>
 );
