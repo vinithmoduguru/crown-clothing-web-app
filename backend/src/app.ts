0bb304cd157111c2ec1b3eac16828426ad7d4ca4
vinithmoduguru <vinith1142@gmail.com>
2025-02-24 23:52:44 +0530

    Added CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db";
 import categoryRoutes from "./routes/categoryRoutes";
 import productRoutes from "./routes/productRoutes";
@@ -16,6 +17,12 @@ const app = express();
 
 // Middleware for JSON body parsing
 app.use(express.json());
+
+// Middleware for CORS
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 
 // Routes
 app.use("/api/categories", categoryRoutes);
